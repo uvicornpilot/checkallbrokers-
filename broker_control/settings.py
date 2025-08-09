ae6c4ab3b12d1ee99ec758f38ef536a9733029d6
CyberPanel <support@cyberpanel.net>
2025-08-09 18:46:37 +0000

    ipdate

--- broker_control/settings.py
+++ broker_control/settings.py
@@ -87,10 +87,20 @@ WSGI_APPLICATION = 'broker_control.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',  # або хост, який дав хостинг
+        'PORT': '5432',       # або інший, якщо нестандартний
     }
 }
 
