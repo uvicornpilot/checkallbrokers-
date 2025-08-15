af0b09456b644810473c8d0ee13ef3a4e0aaf78c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-15 17:31:12 +0300

    test

--- broker_control/settings.py
+++ broker_control/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['forum-broker.com', 'localhost', '127.0.0.1','www.forum-broker.com']
 
