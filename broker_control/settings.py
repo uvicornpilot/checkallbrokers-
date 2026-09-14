625fc55654d00396b28c860c99ca3735b62b1294
Even <soloviov.ivan.17@gmail.com>
2026-09-14 10:45:00 +0300

    content: resize photo and update FAQ section

--- broker_control/settings.py
+++ broker_control/settings.py
@@ -27,11 +27,15 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 #ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = [
+    'https://checkallbrokers.com',
+    'http://127.0.0.1',
+    'http://localhost',
+]
 
 
 # Application definition
